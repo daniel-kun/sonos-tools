f7449d88c40f8d8571ea6eaf8fb7cccb4e9ac16a
Daniel Albuschat <daniel.albuschat@gira.de>
2019-02-04 22:06:07 +0100

    Implemented skeleton for Google Sign-in. Things left: Actually 'apply' the login in the frontend, store the credentials, save credentials in database and store a cookie'

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -1,8 +1,34 @@
 from flask import Flask
 from flask import render_template
+from flask import make_response
+from flask import request
+from apiclient import discovery
+import httplib2
+from oauth2client import client
+import os
 app = Flask(__name__)
+
+SONOSTOOLS_GOOGLE_AUTH_CLIENT_ID=os.environ['SONOSTOOLS_GOOGLE_AUTH_CLIENT_ID']
+SONOSTOOLS_GOOGLE_AUTH_CLIENTFILE=os.environ['SONOSTOOLS_GOOGLE_AUTH_CLIENTFILE']
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', google_auth_client_id=SONOSTOOLS_GOOGLE_AUTH_CLIENT_ID)
+
+@app.route("/receive_google_auth", methods=['POST'])
+def receive_google_auth():
+    payload = request.json
+    print(payload)
+    # Exchange auth code for access token, refresh token, and ID token
+    credentials = client.credentials_from_clientsecrets_and_code(
+            SONOSTOOLS_GOOGLE_AUTH_CLIENTFILE,
+            ['profile', 'email'],
+            payload['authCode'])
+
+    # Get profile info from ID token
+    print(credentials)
+    print(credentials.id_token)
+    userid = credentials.id_token['sub']
+    email = credentials.id_token['email']
+    return make_response('Success', 200)
 
