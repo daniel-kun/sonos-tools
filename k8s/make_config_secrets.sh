95cfc64c93e9ffb5a41f74bb81199343ced5aa35
Daniel Albuschat <daniel.albuschat@gira.de>
2019-02-09 23:00:03 +0000

    Made new version of frontend deployable

--- k8s/make_config_secrets.sh
+++ k8s/make_config_secrets.sh
@@ -6,6 +6,7 @@ kubectl create configmap secrets \
     --from-literal=sonos-tools.mongodb_connecturi=$SONOSTOOLS_MONGODB_CONNECTURI \
     --from-literal=sonos-tools.google_auth_client_id=$SONOSTOOLS_GOOGLE_AUTH_CLIENT_ID \
     --from-literal=sonos-tools.cluster_public_root=$SONOSTOOLS_CLUSTER_PUBLIC_ROOT \
+    --from-literal=sonos-tools.redirect_root=$SONOSTOOLS_CLUSTER_PUBLIC_ROOT \
     --from-literal=sonos-tools.sonosapi_appkey=$SONOSTOOLS_SONOSAPI_APPKEY \
     --from-literal=sonos-tools.sonosapi_secret=$SONOSTOOLS_SONOSAPI_SECRET \
 
