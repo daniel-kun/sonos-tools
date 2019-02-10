beb32df6a476e91f75119b3fa8c31c950ac9c26f
Daniel Albuschat <daniel.albuschat@gira.de>
2019-02-10 21:57:36 +0100

    Applied new, fancy style

--- src/frontend/js/landing.jsx
+++ src/frontend/js/landing.jsx
@@ -6,7 +6,7 @@ import ReactDOM from 'react-dom';
 function renderRoot(account, isSonosSignedIn, sonosApiAppKey, redirectUriRoot)
 {
     console.log("renderRoot")
-   ReactDOM.render(renderLanding(account, isSonosSignedIn, sonosApiAppKey, redirectUriRoot), document.getElementById('landing'))
+   ReactDOM.render(render(account, isSonosSignedIn, sonosApiAppKey, redirectUriRoot), document.getElementById('landing'))
 }
 
 function responseGoogleLogin(googleUser)
@@ -87,65 +87,84 @@ function sonosPlayTest(apiKey)
     }).then(response => console.log(response))
 }
 
-function renderLanding(account, isSonosSignedIn, sonosApiAppKey, redirectUriRoot)
+function renderLanding()
+{
+    return (<div className="landing">
+            <h1>SONOS</h1>
+            <h2>speaks!</h2>
+            <p className="landing-introduction">Integrate your SONOS speakers into your<br/>
+home automation.</p>
+            <p className="landing-introduction">Get notified of events, in high fidelity.</p>
+            <img className="speaker-left" src="/static/img/sonos-bubble-left.svg"/>
+            <img className="speaker-right" src="/static/img/sonos-bubble-right.svg"/>
+            
+            <GoogleLogin 
+                clientId="166334197578-sem3ib4jfiqm8k59npc1s3ddrro5f5bs.apps.googleusercontent.com"
+                isSignedIn={true}
+                onSuccess={responseGoogleLogin}
+                onFailure={responseGoogleFailure}/>
+        </div>)
+}
+
+function renderLoggedInWithoutSonos(sonosApiAppKey, accountid, redirectUriRoot)
+{
+    return (<div className="landing">
+            <h1>SONOS</h1>
+            <h2>speaks!</h2>
+            <p className="landing-introduction">Integrate your SONOS speakers into your<br/>
+home automation.</p>
+            <p className="landing-introduction">Get notified of events, in high fidelity.</p>
+            <img className="speaker-left" src="/static/img/sonos-bubble-left.svg"/>
+            <img className="speaker-right" src="/static/img/sonos-bubble-right.svg"/>
+            
+            <p className="connect-sonos"><button className="sonos connect" onClick={() => { location.href = createSonosAuthUri(sonosApiAppKey, accountid, redirectUriRoot + "/sonos_auth") }}>Connect with SONOS</button></p>
+            <GoogleLogout className="google-logout" onLogoutSuccess={responseGoogleLogout}/>
+        </div>)
+}
+
+function renderLoggedIn(account, isSonosSignedIn, sonosApiAppKey, redirectUriRoot)
 {
     var accountid = account != null && 'accountid' in account ?  accountid = account['accountid'] : null
     var isGoogleSignedIn = accountid != null
-    console.log(`renderLanding(${account}, ${isSonosSignedIn}, ${sonosApiAppKey}, ${redirectUriRoot})`)
-    return (<div>
-            <div className="top-bar secondary"/>
-            <div className="landing">
-                <h1>SONOS</h1>
-                <h2>speaks!</h2>
-                <p className="landing-introduction">Integrate your SONOS speakers into your<br/>
-home automation.</p>
-                <p className="landing-introduction">Get notified of events, in high fidelity.</p>
-                <img class="speaker-left" src="/static/img/sonos-bubble-left.svg"/>
-                <img class="speaker-right" src="/static/img/sonos-bubble-right.svg"/>
-                
-                {/*
-                <GoogleLogin 
-                    clientId="166334197578-sem3ib4jfiqm8k59npc1s3ddrro5f5bs.apps.googleusercontent.com"
-                    isSignedIn={true}
-                    onSuccess={responseGoogleLogin}
-                    onFailure={responseGoogleFailure}/>
-                    */}
-            </div>
-            <div className="section-two secondary">
-                <ol>
-                    <li>
-                    {!isGoogleSignedIn && 
-                        <GoogleLogin 
-                            clientId="166334197578-sem3ib4jfiqm8k59npc1s3ddrro5f5bs.apps.googleusercontent.com"
-                            isSignedIn={true}
-                            onSuccess={responseGoogleLogin}
-                            onFailure={responseGoogleFailure}/>}
-                    {isGoogleSignedIn && <GoogleLogout onLogoutSuccess={responseGoogleLogout}/>}
-                    </li>
-                    {!isGoogleSignedIn && !isSonosSignedIn && <li>Connect with Sonos to receive an API key</li>}
-                    {isGoogleSignedIn && !isSonosSignedIn && <li><a href={createSonosAuthUri(sonosApiAppKey, accountid, redirectUriRoot + "/sonos_auth")}>Connect with Sonos</a> to receive an API key</li>}
-                    {isGoogleSignedIn && isSonosSignedIn && <li>Sonos is signed in (<a href='#' onClick={sonosLogout.bind(null, account, sonosApiAppKey, redirectUriRoot)}>log out</a>)</li>}
-                    <li>HTTP POST the text that you want your speakers to say.</li>
-                </ol>
+    return (<div className="content">
+            <h2>Sonos speaks!</h2>
+            {account.sonos.players.length == 0 && <div><p>We have not found any players.</p><p>Go shopping and come back when you bought and installed a new SONOS player ;-)</p></div>}
+            {account.sonos.players.length == 1 && <span>We have found 1 player.</span>}
+            {account.sonos.players.length > 1 && <span>We have found {account.sonos.players.length} players.</span>}
+            <table>
+                <tbody>
+                    {account.sonos.players.map(player => {
+                        return (
+                            <tr key={`player_${player.playerId}`}>
+                                <td>
+                                    <p className="player-name">Player „{player.playerId}”</p>
+                                    <p className="api-key-label">Your API key for this player is:</p>
+                                    <div>
+                                        <span className="api-key">{player.apiKey}</span>
+                                        <button className="sonos test-button" onClick={sonosPlayTest.bind(this, player.apiKey)}>Test now</button>
+                                    </div>
+                                </td>
+                            </tr>)
+                    })}
+                </tbody>
+            </table>
+            <img className="speaker-right corner" src="/static/img/sonos-bubble-right.svg"/>
+            <GoogleLogout className="google-logout" onLogoutSuccess={responseGoogleLogout}/>
+            <a className="unlink" href="#" onClick={sonosLogout.bind(null, account, sonosApiAppKey, redirectUriRoot)}>unlink this SONOS account</a>
+        </div>)
+}
 
-                {account != null && 'sonos' in account && 'players' in account.sonos &&
-                    <table>
-                        <tbody>
-                            {account.sonos.players.map(player => {
-                                return (
-                                    <tr key={`player_${player.playerId}`}>
-                                        <td>
-                                            <p>Name: {player.playerId}</p>
-                                            <p>API Key: {player.apiKey}</p>
-                                        </td>
-                                        <td>
-                                            <a href='#' onClick={sonosPlayTest.bind(this, player.apiKey)}>Test now</a>
-                                        </td>
-                                    </tr>)
-                            })}
-                        </tbody>
-                    </table>}
-            </div>
+function render(account, isSonosSignedIn, sonosApiAppKey, redirectUriRoot)
+{
+    var accountid = account != null && 'accountid' in account ?  accountid = account['accountid'] : null
+    var isGoogleSignedIn = accountid != null
+    console.log(`render(${account}, ${isSonosSignedIn}, ${sonosApiAppKey}, ${redirectUriRoot})`)
+    return (<div className="root inner">
+            <div className="top-bar secondary"/>
+            {!isGoogleSignedIn && renderLanding()}
+            {isGoogleSignedIn && !isSonosSignedIn && renderLoggedInWithoutSonos(sonosApiAppKey, accountid, redirectUriRoot)}
+            {isGoogleSignedIn && isSonosSignedIn && renderLoggedIn(account, isSonosSignedIn, sonosApiAppKey, redirectUriRoot)}
+            <div className="bottom-bar secondary"/>
         </div>)
 }
 
