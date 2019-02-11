e2a3fcfa8c16588ec20cae1a2ec78ba074631d96
Daniel Albuschat <daniel.albuschat@gira.de>
2019-02-11 22:03:22 +0000

    api.tts: Made the mongodb connection failsafe

--- src/api.tts/app.py
+++ src/api.tts/app.py
@@ -14,6 +14,14 @@ SONOSTOOLS_GCP_API_KEY = os.environ['SONOSTOOLS_GCP_API_KEY']
 SONOSTOOLS_MONGODB_CONNECTURI = os.environ['SONOSTOOLS_MONGODB_CONNECTURI']
 SONOSTOOLS_CLUSTER_PUBLIC_ROOT = os.environ['SONOSTOOLS_CLUSTER_PUBLIC_ROOT']
 
+_dbClient = None
+
+def dbClient():
+    global _dbClient
+    if _dbClient == None:
+        _dbClient = db.connect(SONOSTOOLS_MONGODB_CONNECTURI)
+    return _dbClient
+
 def getTextToSpeechHash(client, languageCode, text, apiKey):
     audioConfigHash = tts.textToAudioConfigHash(languageCode, text)
     audio = db.find_audio(client, audioConfigHash)
@@ -27,26 +35,23 @@ def getTextToSpeechHash(client, languageCode, text, apiKey):
 @app.route("/api/v1/synthesize", methods=['POST'])
 def synthesize():
     try:
-        dbClient = db.connect(SONOSTOOLS_MONGODB_CONNECTURI)
         payload = request.json 
         languageCode = payload['languagecode']
         text = payload['text']
-        apiKey = db.find_apikey(dbClient, payload['apikey'])
+        apiKey = db.find_apikey(dbClient(), payload['apikey'])
         if apiKey == None:
             return make_response('Invalid apikey', 401)
     except Exception as err:
         print(err)
         return make_response('Invalid request', 500)
-    (audioConfigHash, fromCache) = getTextToSpeechHash(dbClient, languageCode, text, SONOSTOOLS_GCP_API_KEY)
+    (audioConfigHash, fromCache) = getTextToSpeechHash(dbClient(), languageCode, text, SONOSTOOLS_GCP_API_KEY)
     return jsonify(audioConfigHash=audioConfigHash, fromCache=fromCache, uri='{0}/audio/{1}'.format(SONOSTOOLS_CLUSTER_PUBLIC_ROOT, audioConfigHash))
 
 @app.route("/audio/<audioConfigHash>")
 def audioFile(audioConfigHash):
-    audioFile = db.find_audio(dbClient, audioConfigHash)
+    audioFile = db.find_audio(dbClient(), audioConfigHash)
     if audioFile != None:
         return make_response((audioFile, 200, {'Content-Type': 'audio/mpeg'}))
     else:
         return make_response("<html><body><h1>Could not find this audio file</h1>", 404)
-
-dbClient = db.connect(SONOSTOOLS_MONGODB_CONNECTURI)
 
