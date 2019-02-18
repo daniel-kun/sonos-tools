4f2d27ee434d71f00d7218efc66bfbe66858cccd
Daniel Albuschat <daniel.albuschat@gira.de>
2019-02-18 18:33:06 +0000

    deploy: Deployed frontend v14, with new cookie notify

--- k8s/make_config_secrets.sh
+++ k8s/make_config_secrets.sh
@@ -9,6 +9,7 @@ kubectl create configmap secrets \
     --from-literal=sonos-tools.redirect_root=$SONOSTOOLS_CLUSTER_PUBLIC_ROOT \
     --from-literal=sonos-tools.sonosapi_appkey=$SONOSTOOLS_SONOSAPI_APPKEY \
     --from-literal=sonos-tools.sonosapi_secret=$SONOSTOOLS_SONOSAPI_SECRET \
+    --from-literal=sonos-tools.flask_secret="$SONOSTOOLS_FLASK_SECRET_KEY" \
 
 kubectl delete configmap/secret-google-auth-clientfile
 kubectl create configmap secret-google-auth-clientfile --from-file=$SONOSTOOLS_GOOGLE_AUTH_CLIENTFILE
