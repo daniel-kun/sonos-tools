f4d17dc6072ce491f15d8e62ed9931d54bdf8292
Daniel Albuschat <daniel.albuschat@gira.de>
2019-03-15 14:01:55 +0100

    tests: Started to implement groups api for Sonos mock

--- tests/mocks/sonos_mock/app.py
+++ tests/mocks/sonos_mock/app.py
@@ -16,26 +16,127 @@ https://api.ws.sonos.com/control/api/v1/players/{0}/audioClip
         "appId": "com.acme.com",
         "streamUrl": uri 
     }
+'''
 
+def get_bearer_token(header):
+    if header.startswith('Bearer '):
+        return header[len('Bearer '):]
+    else:
+        return "";
 
-https://api.sonos.com/login/v3/oauth/access
-    headers = { "Authorization": "Bearer {0}".format(sonosAccessToken) }
-    {
-        "grant_type": "refresh_token",
-        "refresh_token": sonosRefreshToken
-    }
-    ///
-    auth=(SONOSTOOLS_SONOSAPI_APPKEY, SONOSTOOLS_SONOSAPI_SECRET)
-    {
-        "grant_type": "authorization_code",
-        "code": sonosAuthCode,
-        "redirect_uri": '{0}/sonos_auth'.format(SONOSTOOLS_REDIRECT_ROOT)
-    }
-
-
+'''
 https://api.ws.sonos.com/control/api/v1/households/{0}/groups
     headers = { "Authorization": "Bearer {0}".format(sonosAccessToken) }
 '''
+@app.route("/control/api/v1/households/<string:householdId>/groups", methods=["GET"])
+def control_api_v1_households_groups(householdId):
+    token = get_bearer_token(request.headers['Authorization'])
+    if token == "XXX_TOKEN_NO_HOUSEHOLDS":
+        return jsonify({ "households": []})
+    elif token == "XXX_TOKEN_ONE_HOUSEHOLD":
+        if householdId == "XXX_HOUSEHOLD_ONE_1":
+            return jsonify({
+                "players": [
+                    {
+                        "id": "RINCON_10001",
+                        "name": "Wohnzimmer",
+                        "capabilities": [
+                            "AIRPLAY",
+                            "AUDIO_CLIP",
+                            "CLOUD",
+                        ]
+                    }
+                ]
+            })
+        else:
+            return jsonify({"errorCode": "ERROR_RESOURCE_GONE"}, 410)
+    elif token == "XXX_TOKEN_TWO_HOUSEHOLDs":
+        if householdId == "XXX_HOUSEHOLD_TWO_1":
+            return jsonify({
+                "players": [
+                    {
+                        "id": "RINCON_10001",
+                        "name": "Wohnzimmer",
+                        "capabilities": [
+                            "AIRPLAY",
+                            "AUDIO_CLIP",
+                            "CLOUD",
+                        ]
+                    }
+                ]
+            })
+        elif: householdId == "XXX_HOUSEHOLD_TWO_2":
+            return jsonify({
+                "players": [
+                    {
+                        "id": "RINCON_10002",
+                        "name": "Wohnzimmer",
+                        "capabilities": [
+                            "AIRPLAY",
+                            "CLOUD",
+                        ]
+                    }
+                ]
+            })
+        else:
+            return jsonify({"errorCode": "ERROR_RESOURCE_GONE"}, 410)
+    elif token == "XXX_TOKEN_MANY_HOUSEHOLDS":
+        pass # TODO
+             # "id": "XXX_HOUSEHOLD_MANY_1",
+             # "id": "XXX_HOUSEHOLD_MANY_2",
+             # "id": "XXX_HOUSEHOLD_MANY_3",
+             # "id": "XXX_HOUSEHOLD_MANY_4",
+             # "id": "XXX_HOUSEHOLD_MANY_5",
+    else:
+        return make_response('Invalid access_token', 401)
+
+'''
+https://api.ws.sonos.com/control/api/v1/households
+    headers = { "Authorization": "Bearer {0}".format(sonosAccessToken) }
+'''
+@app.route("/control/api/v1/households", methods=["GET"])
+def control_api_v1_houesholds():
+    token = get_bearer_token(request.headers['Authorization'])
+    if token == "XXX_TOKEN_NO_HOUSEHOLDS":
+        return jsonify({ "households": []})
+    elif token == "XXX_TOKEN_ONE_HOUSEHOLD":
+        return jsonify({
+            "households": [
+                    {
+                        "id": "XXX_HOUSEHOLD_ONE_1",
+                    }
+                ]})
+    elif token == "XXX_TOKEN_TWO_HOUSEHOLDs":
+        return jsonify({
+            "households": [
+                    {
+                        "id": "XXX_HOUSEHOLD_TWO_1",
+                    },
+                    {
+                        "id": "XXX_HOUSEHOLD_TWO_2",
+                    },
+                ]})
+    elif token == "XXX_TOKEN_MANY_HOUSEHOLDS":
+        return jsonify({
+            "households": [
+                    {
+                        "id": "XXX_HOUSEHOLD_MANY_1",
+                    },
+                    {
+                        "id": "XXX_HOUSEHOLD_MANY_2",
+                    },
+                    {
+                        "id": "XXX_HOUSEHOLD_MANY_3",
+                    },
+                    {
+                        "id": "XXX_HOUSEHOLD_MANY_4",
+                    },
+                    {
+                        "id": "XXX_HOUSEHOLD_MANY_5",
+                    },
+                ]})
+    else:
+        return make_response('Invalid access_token', 401)
 
 def check_auth(header, username, password):
     print(header)
@@ -47,62 +148,6 @@ def check_auth(header, username, password):
         return decoded_creds == '{0}:{1}'.format(username, password).encode('ASCII')
     else:
         return False
-
-def get_bearer_token(header):
-    if header.startswith('Bearer '):
-        return header[len('Bearer '):]
-    else:
-        return "";
-
-
-'''
-https://api.ws.sonos.com/control/api/v1/households
-    headers = { "Authorization": "Bearer {0}".format(sonosAccessToken) }
-'''
-@app.route("/control/api/v1/households", methods=["GET"])
-def control_api_v1_houesholds():
-    token = get_bearer_token(request.headers['Authorization'])
-    if token == "XXX_TOKEN_NO_HOUSEHOLDS":
-        return jsonify({
-            "households": []})
-    elif token == "XXX_TOKEN_ONE_HOUSEHOLD":
-        return jsonify({
-            "households": [
-                    {
-                        "id": "XXX_HOUSEHOLD_1",
-                    }
-                ]})
-    elif token == "XXX_TOKEN_TWO_HOUSEHOLDs":
-        return jsonify({
-            "households": [
-                    {
-                        "id": "XXX_HOUSEHOLD_1",
-                    },
-                    {
-                        "id": "XXX_HOUSEHOLD_2",
-                    },
-                ]})
-    elif token == "XXX_TOKEN_MANY_HOUSEHOLDS":
-        return jsonify({
-            "households": [
-                    {
-                        "id": "XXX_HOUSEHOLD_1",
-                    },
-                    {
-                        "id": "XXX_HOUSEHOLD_2",
-                    },
-                    {
-                        "id": "XXX_HOUSEHOLD_3",
-                    },
-                    {
-                        "id": "XXX_HOUSEHOLD_4",
-                    },
-                    {
-                        "id": "XXX_HOUSEHOLD_5",
-                    },
-                ]})
-    else:
-        return make_response('Invalid access_token', 401)
 
 @app.route("/login/v3/oauth/access", methods=['POST'])
 def login_v3_oauth_access():
