3f4569eb457af0485ef793f6469f4e5cf75a492c
Daniel Albuschat <daniel.albuschat@gira.de>
2019-04-19 13:52:29 +0000

    deploy: Added script to deploy to production

---
+++ deploy_production.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source ~/.sonostools-prod.env
+
+args=( --set Env="${SONOSTOOLS_ENV}" \
+    --set ClusterPublicIP="${SONOSTOOLS_CLUSTER_PUBLIC_IP}" \
+    --set ClusterPublicRoot="${SONOSTOOLS_CLUSTER_PUBLIC_ROOT}" \
+    --set DockerRepo="${SONOSTOOLS_DOCKER_REPO}" \
+    --set DockerImagePrefix="${SONOSTOOLS_DOCKER_IMAGE_PREFIX}" \
+    --set FlaskSecret="${SONOSTOOLS_FLASK_SECRET_KEY}" \
+    --set GcpApiKey="${SONOSTOOLS_GCP_API_KEY}" \
+    --set GoogleAuthClientId="${SONOSTOOLS_GOOGLE_AUTH_CLIENT_ID}" \
+    --set MongoDBConnectUri="${SONOSTOOLS_MONGODB_CONNECTURI}" \
+    --set RedirectRoot="${SONOSTOOLS_REDIRECT_ROOT}" \
+    --set SonosApiEndpoint="${SONOSTOOLS_SONOSAPI_ENDPOINT}" \
+    --set SonosApiEndpointWS="${SONOSTOOLS_SONOSAPI_ENDPOINT_WS}" \
+    --set SonosApiAppKey="${SONOSTOOLS_SONOSAPI_APPKEY}" \
+    --set SonosApiAppSecret="${SONOSTOOLS_SONOSAPI_SECRET}" \
+    --set-file TlsCert="${SONOSTOOLS_TLS_CERTFILE}"  \
+    --set-file TlsKey="${SONOSTOOLS_TLS_PRIVKEY}" \
+    );
+
+helm list | grep sonos-tools && \
+    helm upgrade sonos-tools-production charts/sonos-tools "${args[@]}" || \
+    helm install charts/sonos-tools --name sonos-tools-production "${args[@]}" 
+
+
