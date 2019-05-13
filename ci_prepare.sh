977e9706e3cf48c649553f4d596b65ae1daed2cf
Daniel Albuschat <d.albuschat@gmail.com>
2019-05-13 23:14:34 +0200

    Testing circleci

---
+++ ci_prepare.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SONOSTOOLS_KUBECONFIG=${HOME}/.ci_tmpfiles/kube_config
+SONOSTOOLS_TLS_CERTFILE=${HOME}/.ci_tmpfiles/cert
+SONOSTOOLS_TLS_PRIVKEY=${HOME}/.ci_tmpfiles/privkey
+
+rm -rvf ${HOME}/.ci_tmpfiles/
+mkdir ${HOME}/.ci_tmpfiles
+
+echo "${SONOSTOOLS_KUBECONFIG_BASE64}" | base64 -d > ${SONOSTOOLS_KUBECONFIG}
+echo "${SONOSTOOLS_TLS_CERTFILE_BASE64}" | base64 -d > ${SONOSTOOLS_TLS_CERTFILE}
+echo "${SONOSTOOLS_TLS_PRIVKEY_BASE64}" | base64 -d > ${SONOSTOOLS_TLS_PRIVKEY}
+
