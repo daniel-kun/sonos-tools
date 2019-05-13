43603a7eaa6e8a0037cd13ffd075aa5043fa55ff
Daniel Albuschat <d.albuschat@gmail.com>
2019-05-13 23:32:53 +0200

    ...

--- ci_prepare.sh
+++ ci_prepare.sh
@@ -8,10 +8,14 @@ rm -rvf ${HOME}/.ci_tmpfiles/
 mkdir ${HOME}/.ci_tmpfiles
 
 # Just for testing:
-echo "Inside ci_prepare.sh:
-${SONOSTOOLS_KUBECONFIG_BASE64}"
 
+echo "================== 1"
 echo "${SONOSTOOLS_KUBECONFIG_BASE64}" | base64 -d > ${SONOSTOOLS_KUBECONFIG}
+cat ${SONOSTOOLS_KUBECONFIG}
+echo "================== 2"
 echo "${SONOSTOOLS_TLS_CERTFILE_BASE64}" | base64 -d > ${SONOSTOOLS_TLS_CERTFILE}
+cat ${SONOSTOOLS_TLS_CERTFILE}
+echo "================== 3"
 echo "${SONOSTOOLS_TLS_PRIVKEY_BASE64}" | base64 -d > ${SONOSTOOLS_TLS_PRIVKEY}
+cat ${SONOSTOOLS_TLS_PRIVKEY}
 
