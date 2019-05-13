0d9956d0bbb8e04374baf8daeb14ac1ccc81b406
Daniel Albuschat <d.albuschat@gmail.com>
2019-05-13 23:25:27 +0200

    Testing env vars in circle ci

--- ci_prepare.sh
+++ ci_prepare.sh
@@ -7,6 +7,10 @@ SONOSTOOLS_TLS_PRIVKEY=${HOME}/.ci_tmpfiles/privkey
 rm -rvf ${HOME}/.ci_tmpfiles/
 mkdir ${HOME}/.ci_tmpfiles
 
+# Just for testing:
+echo "Inside ci_prepare.sh:
+${SONOSTOOLS_KUBECONFIG_BASE64}"
+
 echo "${SONOSTOOLS_KUBECONFIG_BASE64}" | base64 -d > ${SONOSTOOLS_KUBECONFIG}
 echo "${SONOSTOOLS_TLS_CERTFILE_BASE64}" | base64 -d > ${SONOSTOOLS_TLS_CERTFILE}
 echo "${SONOSTOOLS_TLS_PRIVKEY_BASE64}" | base64 -d > ${SONOSTOOLS_TLS_PRIVKEY}
