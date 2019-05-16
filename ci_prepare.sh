57bb1e2e4c75930cf3a857a33900adc7eed4d3ea
Daniel Albuschat <d.albuschat@gmail.com>
2019-05-16 10:51:30 +0200

    CI: Trying to build and push images

--- ci_prepare.sh
+++ ci_prepare.sh
@@ -13,7 +13,12 @@ echo "${SONOSTOOLS_KUBECONFIG_BASE64}" | base64 -d > ${SONOSTOOLS_KUBECONFIG}
 echo "${SONOSTOOLS_TLS_CERTFILE_BASE64}" | base64 -d > ${SONOSTOOLS_TLS_CERTFILE}
 echo "${SONOSTOOLS_TLS_PRIVKEY_BASE64}" | base64 -d > ${SONOSTOOLS_TLS_PRIVKEY}
 
-md5sum ${SONOSTOOLS_KUBECONFIG}
-md5sum ${SONOSTOOLS_TLS_CERTFILE}
-md5sum ${SONOSTOOLS_TLS_PRIVKEY}
+echo "kubeconf md5: `md5sum ${SONOSTOOLS_KUBECONFIG}`"
+echo "TLS cert md5: `md5sum ${SONOSTOOLS_TLS_CERTFILE}`"
+echo "TLS key  md5: `md5sum ${SONOSTOOLS_TLS_PRIVKEY}`"
+
+echo "Logging into docker:"
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+echo "Building and pushing docker images:"
+./build.sh
 
